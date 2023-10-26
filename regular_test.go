f18f1abcce1aa1eaaf9f6362a92f94872c893104
Andrey Zinenko <zinenkoan@yahoo.com>
2023-10-26 23:22:20 +0300

    improve test coverage for trie.Match

--- regular_test.go
+++ regular_test.go
@@ -153,222 +153,725 @@ func TestTrieCompression(t *testing.T) {
 }
 
 func TestMatch(t *testing.T) {
-	t.Run("simple", func(t *testing.T) {
-		tr, err := NewTrie(
-			"te",
-			"toast",
-			"toaster",
-			"word",
-			"strong",
-			"wizard",
-			"test",
-			"string",
-			"s",
-			"ing",
-		)
-		require.NoError(t, err)
-
-		t.Log(tr.String())
-
-		examples := map[string][]*FullMatch{
-			"testing string test ssss word words": {
-
-				// [te test s ing s string ing te test s s s s s word word s]
-				{
-					subString: "te",
-					from:      0,
-					to:        1,
-					expressions: []string{
-						"te",
-					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
+	examples := map[string][]example{
+		"simple": {
+			{
+				name: "match chars as is",
+				regexps: []string{
+					"te",
+					"toast",
+					"toaster",
+					"word",
+					"strong",
+					"wizard",
+					"test",
+					"string",
+					"s",
+					"ing",
 				},
-				{
-					subString: "test",
-					from:      0,
-					to:        3,
-					expressions: []string{
-						"test",
+				input: "testing string test ssss word words",
+				output: []*FullMatch{
+					{
+						subString: "te",
+						from:      0,
+						to:        1,
+						expressions: []string{
+							"te",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      2,
-					to:        2,
-					expressions: []string{
-						"s",
+					{
+						subString: "test",
+						from:      0,
+						to:        3,
+						expressions: []string{
+							"test",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "ing",
-					from:      4,
-					to:        6,
-					expressions: []string{
-						"ing",
+					{
+						subString: "s",
+						from:      2,
+						to:        2,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      8,
-					to:        8,
-					expressions: []string{
-						"s",
+					{
+						subString: "ing",
+						from:      4,
+						to:        6,
+						expressions: []string{
+							"ing",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "string",
-					from:      8,
-					to:        13,
-					expressions: []string{
-						"string",
+					{
+						subString: "s",
+						from:      8,
+						to:        8,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "ing",
-					from:      11,
-					to:        13,
-					expressions: []string{
-						"ing",
+					{
+						subString: "string",
+						from:      8,
+						to:        13,
+						expressions: []string{
+							"string",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "te",
-					from:      15,
-					to:        16,
-					expressions: []string{
-						"te",
+					{
+						subString: "ing",
+						from:      11,
+						to:        13,
+						expressions: []string{
+							"ing",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "test",
-					from:      15,
-					to:        18,
-					expressions: []string{
-						"test",
+					{
+						subString: "te",
+						from:      15,
+						to:        16,
+						expressions: []string{
+							"te",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      17,
-					to:        17,
-					expressions: []string{
-						"s",
+					{
+						subString: "test",
+						from:      15,
+						to:        18,
+						expressions: []string{
+							"test",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      20,
-					to:        20,
-					expressions: []string{
-						"s",
+					{
+						subString: "s",
+						from:      17,
+						to:        17,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      21,
-					to:        21,
-					expressions: []string{
-						"s",
+					{
+						subString: "s",
+						from:      20,
+						to:        20,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      22,
-					to:        22,
-					expressions: []string{
-						"s",
+					{
+						subString: "s",
+						from:      21,
+						to:        21,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      23,
-					to:        23,
-					expressions: []string{
-						"s",
+					{
+						subString: "s",
+						from:      22,
+						to:        22,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "word",
-					from:      25,
-					to:        28,
-					expressions: []string{
-						"word",
+					{
+						subString: "s",
+						from:      23,
+						to:        23,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "word",
-					from:      30,
-					to:        33,
-					expressions: []string{
-						"word",
+					{
+						subString: "word",
+						from:      25,
+						to:        28,
+						expressions: []string{
+							"word",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
-				},
-				{
-					subString: "s",
-					from:      34,
-					to:        34,
-					expressions: []string{
-						"s",
+					{
+						subString: "word",
+						from:      30,
+						to:        33,
+						expressions: []string{
+							"word",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
 					},
-					namedGroups: map[string]bounds{},
-					groups:      []bounds{},
+					{
+						subString: "s",
+						from:      34,
+						to:        34,
+						expressions: []string{
+							"s",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
 				},
 			},
-		}
+			{
+				name:    "match '.' case 1",
+				regexps: []string{"t."},
+				input:   "testing string test ssss word words",
+				output: []*FullMatch{
+					{
+						subString: "te",
+						from:      0,
+						to:        1,
+						expressions: []string{
+							"t.",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "ti",
+						from:      3,
+						to:        4,
+						expressions: []string{
+							"t.",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "tr",
+						from:      9,
+						to:        10,
+						expressions: []string{
+							"t.",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "te",
+						from:      15,
+						to:        16,
+						expressions: []string{
+							"t.",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "t ",
+						from:      18,
+						to:        19,
+						expressions: []string{
+							"t.",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+			{
+				name:    "match '.' case 2",
+				regexps: []string{"t.."},
+				input:   "testing string test ssss word words",
+				output: []*FullMatch{
+					{
+						subString: "tes",
+						from:      0,
+						to:        2,
+						expressions: []string{
+							"t..",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "tin",
+						from:      3,
+						to:        5,
+						expressions: []string{
+							"t..",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "tri",
+						from:      9,
+						to:        11,
+						expressions: []string{
+							"t..",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "tes",
+						from:      15,
+						to:        17,
+						expressions: []string{
+							"t..",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "t s",
+						from:      18,
+						to:        20,
+						expressions: []string{
+							"t..",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+			{
+				name:    "match '\\d'",
+				regexps: []string{"\\d"},
+				input:   "asd 1 jsdfk 4234",
+				output: []*FullMatch{
+					{
+						subString: "1",
+						from:      4,
+						to:        4,
+						expressions: []string{
+							"\\d",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "4",
+						from:      12,
+						to:        12,
+						expressions: []string{
+							"\\d",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "2",
+						from:      13,
+						to:        13,
+						expressions: []string{
+							"\\d",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "3",
+						from:      14,
+						to:        14,
+						expressions: []string{
+							"\\d",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "4",
+						from:      15,
+						to:        15,
+						expressions: []string{
+							"\\d",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+			{
+				name:    "match '\\D'",
+				regexps: []string{"\\D"},
+				input:   "asd 1 jsdfk 4234",
+				output: []*FullMatch{
+					{
+						subString: "a",
+						from:      0,
+						to:        0,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "s",
+						from:      1,
+						to:        1,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "d",
+						from:      2,
+						to:        2,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: " ",
+						from:      3,
+						to:        3,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: " ",
+						from:      5,
+						to:        5,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "j",
+						from:      6,
+						to:        6,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "s",
+						from:      7,
+						to:        7,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "d",
+						from:      8,
+						to:        8,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "f",
+						from:      9,
+						to:        9,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "k",
+						from:      10,
+						to:        10,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: " ",
+						from:      11,
+						to:        11,
+						expressions: []string{
+							"\\D",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+			{
+				name:    "match '\\w'",
+				regexps: []string{"\\w"},
+				input:   "asd 1 jsdfk 4234",
+				output: []*FullMatch{
+					{
+						subString: "a",
+						from:      0,
+						to:        0,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "s",
+						from:      1,
+						to:        1,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "d",
+						from:      2,
+						to:        2,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "1",
+						from:      4,
+						to:        4,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "j",
+						from:      6,
+						to:        6,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "s",
+						from:      7,
+						to:        7,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "d",
+						from:      8,
+						to:        8,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "f",
+						from:      9,
+						to:        9,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "k",
+						from:      10,
+						to:        10,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "4",
+						from:      12,
+						to:        12,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "2",
+						from:      13,
+						to:        13,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "3",
+						from:      14,
+						to:        14,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "4",
+						from:      15,
+						to:        15,
+						expressions: []string{
+							"\\w",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+			{
+				name:    "match '\\W'",
+				regexps: []string{"\\W"},
+				input:   "asd 1 jsdfk 4234!\n\r",
+				output: []*FullMatch{
+					{
+						subString: " ",
+						from:      3,
+						to:        3,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: " ",
+						from:      5,
+						to:        5,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: " ",
+						from:      11,
+						to:        11,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "!",
+						from:      16,
+						to:        16,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "\n",
+						from:      17,
+						to:        17,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+					{
+						subString: "\r",
+						from:      18,
+						to:        18,
+						expressions: []string{
+							"\\W",
+						},
+						namedGroups: map[string]bounds{},
+						groups:      []bounds{},
+					},
+				},
+			},
+		},
+	}
 
-		for in, ex := range examples {
-			expected := ex
-			input := in
+	for groupName, subGroups := range examples {
+		t.Run(groupName, func(t *testing.T) {
+			for _, ex := range subGroups {
+				test := ex
+				t.Run(ex.name, func(t *testing.T) {
+					tr, err := NewTrie(test.regexps...)
+					require.NoError(t, err)
 
-			sort.SliceStable(expected, func(i, j int) bool {
-				return comparator(expected[i], expected[j])
-			})
+					// t.Log(tr.String())
+					// t.Logf("input: '%s'", string(test.input))
 
-			t.Run(input, func(t *testing.T) {
-				t.Logf("input: '%s'", string(input))
+					sort.SliceStable(test.output, func(i, j int) bool {
+						return comparator(test.output[i], test.output[j])
+					})
 
-				actual := tr.Match(input)
-				require.NoError(t, err)
+					actual := tr.Match(test.input)
+					require.NoError(t, err)
 
-				sort.SliceStable(actual, func(i, j int) bool {
-					return comparator(actual[i], actual[j])
+					sort.SliceStable(actual, func(i, j int) bool {
+						return comparator(actual[i], actual[j])
+					})
+
+					require.EqualValues(t, test.output, actual)
 				})
+			}
+		})
+	}
+}
 
-				require.EqualValues(t, expected, actual)
-			})
-		}
-	})
+type example struct {
+	name    string
+	regexps []string
+	input   string
+	output  []*FullMatch
 }
 
 func comparator(x, y *FullMatch) bool {
