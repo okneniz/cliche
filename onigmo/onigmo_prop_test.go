dffb8bc0ee2e1cb7bddf06d1f94a8ecab1fa671c
Andrey Zinenko <zinenkoan@yahoo.com>
2025-08-28 20:25:29 +0300

    improve arbitrary regexp generator

--- onigmo/onigmo_prop_test.go
+++ onigmo/onigmo_prop_test.go
@@ -14,26 +14,82 @@ import (
 func TestOnigmoProperties(t *testing.T) {
 	t.Parallel()
 
-	const iterations = 1_000_000
-
 	seed := time.Now().UnixNano()
 	t.Logf("seed: %v", seed)
 
-	rnd := rand.New(rand.NewPCG(0, uint64(seed)))
-	arb := tests.ArbitraryRegexp(rnd)
+	t.Run("one by one", func(t *testing.T) {
+		t.Parallel()
 
-	ohsnap.Check(t, iterations, arb, func(reg tests.TestRegexp) bool {
-		tr := cliche.New(onigmo.OnigmoParser)
-		tr.Add(reg.Expression)
-		matches := tr.Match(reg.MatchedString)
+		const iterations = 1000
 
-		if len(matches) == 0 {
-			t.Log("expression", reg.Expression)
-			t.Log("string", reg.MatchedString)
-			return false
-		}
+		rnd := rand.New(rand.NewPCG(0, uint64(seed)))
+		arb := tests.ArbitraryRegexp(rnd, 3, 7)
 
-		return true
+		ohsnap.Check(t, iterations, arb, func(reg tests.TestRegexp) bool {
+			tr := cliche.New(onigmo.OnigmoParser)
+
+			err := tr.Add(reg.Expression)
+			if err != nil {
+				t.Log("add expression", reg.Expression)
+				t.Log("string", reg.MatchedString)
+				t.Error(err)
+
+				return false
+			}
+
+			// t.Log("expression", reg.Expression)
+			// t.Log("string", reg.MatchedString)
+			// t.Log("tree", tr.String())
+
+			matches := tr.Match(reg.MatchedString)
+			if len(matches) == 0 {
+				t.Log("expression", reg.Expression)
+				t.Log("string", reg.MatchedString)
+				return false
+			}
+
+			return true
+		})
 	})
 
+	t.Run("in batch", func(t *testing.T) {
+		t.Parallel()
+
+		const iterations = 100
+
+		rnd := rand.New(rand.NewPCG(0, uint64(seed)))
+		arbExp := tests.ArbitraryRegexp(rnd, 3, 7)
+		arb := ohsnap.ArbitrarySlice(rnd, arbExp, 10, 50)
+
+		ohsnap.Check(t, iterations, arb, func(regs []tests.TestRegexp) bool {
+			tr := cliche.New(onigmo.OnigmoParser)
+
+			for _, reg := range regs {
+				err := tr.Add(reg.Expression)
+				if err != nil {
+					t.Log("add expression", reg.Expression)
+					t.Log("string", reg.MatchedString)
+					t.Error(err)
+
+					return false
+				}
+			}
+
+			for _, reg := range regs {
+				// t.Log("expression", reg.Expression)
+				// t.Log("string", reg.MatchedString)
+				// t.Log("tree", tr.String())
+
+				matches := tr.Match(reg.MatchedString)
+				if len(matches) == 0 {
+					t.Log("expression", reg.Expression)
+					t.Log("string", reg.MatchedString)
+
+					return false
+				}
+			}
+
+			return true
+		})
+	})
 }
--- testing/properties.go
+++ testing/properties.go
@@ -1,12 +1,14 @@
 package testing
 
 import (
+	"fmt"
 	"math/rand/v2"
-	"unicode"
+	"strings"
 
 	ohsnap "github.com/okneniz/oh-snap"
 )
 
+// Simplest regexp generator
 type TestRegexp struct {
 	Expression    string
 	MatchedString string
@@ -15,38 +17,38 @@ type TestRegexp struct {
 type arbitraryRegexp struct {
 	rand *rand.Rand
 
-	min, max int
+	maxDeep int
 
 	arbType ohsnap.Arbitrary[int]
+	arbSize ohsnap.Arbitrary[int]
 	arbRune ohsnap.Arbitrary[rune]
 }
 
-func ArbitraryRegexp(rnd *rand.Rand) ohsnap.Arbitrary[TestRegexp] {
+func ArbitraryRegexp(rnd *rand.Rand, maxDeep, maxSize int) ohsnap.Arbitrary[TestRegexp] {
 	arbRune := ohsnap.OneOf(
 		rnd,
 		[]ohsnap.Arbitrary[rune]{
-			ohsnap.RuneFromTable(rnd, unicode.L),
-			ohsnap.RuneFromTable(rnd, unicode.Digit),
-			ohsnap.RuneFromTable(rnd, unicode.Space),
+			ohsnap.OneOfValue(rnd, []rune("abcdefghijklmnopqrstuvwxyz")...),
+			ohsnap.OneOfValue(rnd, []rune("01234567890")...),
 		},
 	)
 
 	return &arbitraryRegexp{
 		rand:    rnd,
-		min:     1,
-		max:     10,
-		arbType: ohsnap.ArbitraryInt(rnd, 1, 2),
+		maxDeep: maxDeep,
+		arbType: ohsnap.ArbitraryInt(rnd, 1, 1000),
+		arbSize: ohsnap.ArbitraryInt(rnd, 1, maxSize),
 		arbRune: arbRune,
 	}
 }
 
 func (a *arbitraryRegexp) Generate() TestRegexp {
-	size := a.rand.IntN(a.max-a.min+1) + int(a.min)
+	size := a.arbSize.Generate()
 
 	reg := TestRegexp{}
 
 	for i := 0; i < size; i++ {
-		subExp := a.generateExp()
+		subExp := a.generateExp(1)
 
 		reg.Expression += subExp.Expression
 		reg.MatchedString += subExp.MatchedString
@@ -55,22 +57,194 @@ func (a *arbitraryRegexp) Generate() TestRegexp {
 	return reg
 }
 
-func (a *arbitraryRegexp) generateExp() TestRegexp {
-	// switch a.arbType.Generate() {
-	// case 1:
-	char := a.generateChar()
-
-	return TestRegexp{
-		MatchedString: string(char),
-		Expression:    string(char),
+func (a *arbitraryRegexp) generateExp(deep int) TestRegexp {
+	if deep >= a.maxDeep {
+		return a.generateString()
 	}
-	// default:
-	// 	panic("unknown type")
-	// }
+
+	var reg TestRegexp
+
+	switch a.arbType.Generate() % 3 {
+	case 0:
+		reg = a.generateString()
+	case 1:
+		reg = a.generateAlt(deep + 1)
+	case 2:
+		reg = a.generateClass()
+	default:
+		reg = a.generateGroup(deep + 1)
+	}
+
+	return a.optionalQuantifier(reg)
+}
+
+func (a *arbitraryRegexp) optionalQuantifier(reg TestRegexp) TestRegexp {
+	if (a.arbType.Generate() % 10) < 7 {
+		return reg
+	}
+
+	size := 1 + a.arbType.Generate()%5
+
+	switch a.arbType.Generate() % 5 {
+	case 0: // {1}
+		matchedString := strings.Repeat(reg.MatchedString, size)
+
+		return TestRegexp{
+			Expression:    fmt.Sprintf("(%s){%d}", reg.Expression, size),
+			MatchedString: matchedString,
+		}
+	case 1: // {,1}
+		repeats := 1 + rand.IntN(size)
+		matchedString := strings.Repeat(reg.MatchedString, repeats)
+
+		return TestRegexp{
+			Expression:    fmt.Sprintf("(%s){,%d}", reg.Expression, size),
+			MatchedString: matchedString,
+		}
+	case 2: // {1,}
+		repeats := size + rand.IntN(size)
+		matchedString := strings.Repeat(reg.MatchedString, repeats)
+
+		return TestRegexp{
+			Expression:    fmt.Sprintf("(%s){%d,}", reg.Expression, size),
+			MatchedString: matchedString,
+		}
+	case 3: // +
+		repeats := 1 + rand.IntN(size)
+		matchedString := strings.Repeat(reg.MatchedString, repeats)
+
+		return TestRegexp{
+			Expression:    fmt.Sprintf("(%s)+", reg.Expression),
+			MatchedString: matchedString,
+		}
+	case 4: // *
+		repeats := 1 + rand.IntN(size)
+		matchedString := strings.Repeat(reg.MatchedString, repeats)
+
+		return TestRegexp{
+			Expression:    fmt.Sprintf("(%s)*", reg.Expression),
+			MatchedString: matchedString,
+		}
+	case 5: // ?
+		// skip
+	}
+
+	return reg
 }
 
 func (a *arbitraryRegexp) generateChar() rune {
 	return a.arbRune.Generate()
+}
+
+func (a *arbitraryRegexp) generateString() TestRegexp {
+	size := a.arbSize.Generate()
+
+	runes := make([]rune, size)
+	for i := 0; i < size; i++ {
+		runes[i] = a.generateChar()
+	}
+
+	str := string(runes)
+
+	return TestRegexp{
+		MatchedString: str,
+		Expression:    str,
+	}
+}
+
+func (a *arbitraryRegexp) generateAlt(deep int) TestRegexp {
+	deep++
+	size := a.arbSize.Generate()
+
+	matchedIndex := rand.IntN(size)
+	matchedString := ""
+
+	variants := make([]string, size)
+	for i := 0; i < size; i++ {
+		exp := a.generateExp(deep)
+		variants[i] = exp.Expression
+
+		if i == matchedIndex {
+			matchedString = exp.MatchedString
+		}
+	}
+
+	expression := strings.Join(variants, "|")
+
+	return TestRegexp{
+		MatchedString: matchedString,
+		Expression:    fmt.Sprintf("(%s)", expression),
+	}
+}
+
+func (a *arbitraryRegexp) generateGroup(deep int) TestRegexp {
+	exp := a.generateExp(deep + 1)
+
+	return TestRegexp{
+		MatchedString: exp.MatchedString,
+		Expression:    fmt.Sprintf("(%s)", exp.Expression),
+	}
+}
+
+func (a *arbitraryRegexp) generateClass() TestRegexp {
+	size := a.arbSize.Generate()
+
+	matchedString := ""
+	matchedIndex := rand.IntN(size)
+
+	expression := make([]string, size)
+	for i := 0; i < size; i++ {
+		item := a.generateClassItem()
+		expression[i] = item.Expression
+
+		if i == matchedIndex {
+			matchedString = item.MatchedString
+		}
+	}
+
+	return TestRegexp{
+		MatchedString: matchedString,
+		Expression:    fmt.Sprintf("[%s]", strings.Join(expression, "")),
+	}
+}
+
+func (a *arbitraryRegexp) generateClassItem() TestRegexp {
+	switch a.arbType.Generate() % 2 {
+	case 0:
+		return a.generateCharTable()
+	case 1:
+		return a.generateRange()
+	default:
+		panic("unknown item type")
+	}
+}
+
+func (a *arbitraryRegexp) generateCharTable() TestRegexp {
+	char := a.generateChar()
+	str := string(char)
+
+	return TestRegexp{
+		MatchedString: str,
+		Expression:    str,
+	}
+}
+
+func (a *arbitraryRegexp) generateRange() TestRegexp {
+	from := a.generateChar()
+	to := a.generateChar()
+
+	if from > to {
+		x := from
+		from = to
+		to = x
+	}
+
+	matchedString := string(from) // TODO : choose random from range
+
+	return TestRegexp{
+		Expression:    fmt.Sprintf("%s-%s", string(from), string(to)),
+		MatchedString: matchedString,
+	}
 }
 
 func (*arbitraryRegexp) Shrink(value TestRegexp) []TestRegexp {
