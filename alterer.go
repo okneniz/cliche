64ef7427f934cc74db15eed6b2601f443b774f37
Andrey Zinenko <zinenkoan@yahoo.com>
2025-10-03 21:50:46 +0300

    Remove

--- alterer.go
+++ alterer.go
@@ -2,7 +2,6 @@ package cliche
 
 import "github.com/okneniz/cliche/node"
 
-// todo : pass tree, not one node?
 type Alter func(node node.Node) node.Node
 
 func Compose(xs ...Alter) Alter {
@@ -23,20 +22,13 @@ func VariantToNode(n node.Alternation) node.Node {
 	variants := n.GetVariants()
 
 	if len(variants) == 0 {
-		return variants[0] // node.Node
+		return variants[0]
 	}
 
-	return n // node.Alternation
+	return n
 }
 
 // RemovComments - remove comments from chains
 func RemoveComments(n node.Node) node.Node {
-	panic("what?")
+	panic("not implemented yet")
 }
-
-// - translate one type node to another -> \d{3} -> \d\d\d ?
-// TODO : add special type for quantifier?
-
-// - empty chains to special node
-
-// TODO : mark children as leaf on remove parent
